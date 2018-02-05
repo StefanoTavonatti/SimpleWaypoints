3c21ec95e44c9cdc519c4d4b8d2560c994eecdb1
StefanoTavonatti <stefanotavonatti93@gmail.com>
2018-02-05 13:47:33 +0100

    bugfix

--- src/main/java/tavonatti/stefano/spigot_plugin/waypoints/Main.java
+++ src/main/java/tavonatti/stefano/spigot_plugin/waypoints/Main.java
@@ -1,4 +1,17 @@
 package tavonatti.stefano.spigot_plugin.waypoints;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        System.out.println("plugin enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        System.out.println("plugin disabled");
+    }
 }
